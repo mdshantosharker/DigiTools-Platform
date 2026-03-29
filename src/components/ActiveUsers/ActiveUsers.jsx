520b815505a82f2b1a1f6b88f1649c2b49a23010
mdshantosharker <mdshantosharker@gmail.com>
2026-03-30 01:50:49 +0600

    added activeUsers components and toggle buttons

--- src/components/ActiveUsers/ActiveUsers.jsx
+++ src/components/ActiveUsers/ActiveUsers.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const ActiveUsers = () => {
-    return (
-        <div className='border'>
-            50k
+  return (
+    
+    <>
+      <div className="bg-linear-to-bl from-[#9514FA] to-[#4F39F6] text-white p-6 sm:p-8 md:p-10 flex flex-col md:flex-row justify-center items-center gap-10 lg:gap-36 mt-20">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl">50K+</h1>
+          <p className="mt-2 text-sm sm:text-base">Active Users</p>
         </div>
-    );
+
+        <div className="divider md:divider-horizontal before:bg-white after:bg-white"></div>
+
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl">200+</h1>
+          <p className="mt-2 text-sm sm:text-base">Premium Tools</p>
+        </div>
+
+        <div className="divider md:divider-horizontal before:bg-white after:bg-white"></div>
+
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl">4.9</h1>
+          <p className="mt-2 text-sm sm:text-base">Rating</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ActiveUsers;
--- src/components/DigitalTools/DigitalTools.jsx
+++ src/components/DigitalTools/DigitalTools.jsx
@@ -10,30 +10,32 @@ const DigitalTools = ({ productsPromise, carts, setCart }) => {
   //   console.log(carts);
   return (
     <div>
-      <div className="text-center mt-10">
-        <h1 className="font-bold text-2xl">Premium Digital Tools</h1>
-        <p>
+      <div className="text-center mt-16 space-y-4">
+        <h1 className="font-bold text-5xl">Premium Digital Tools</h1>
+        <p className="text-[#627382] text-[16px]">
           Choose from our curated collection of premium digital products
-          designed to boost your productivity and creativity.
+          designed <br /> to boost your productivity and creativity.
         </p>
       </div>
 
-      <div className="tabs tabs-box justify-center bg-white mt-10">
-        <input
-          onClick={() => setActive("products")}
-          type="radio"
-          name="my_tabs_1"
-          className="tab"
-          aria-label="Products"
-          defaultChecked
-        />
-        <input
-          onClick={() => setActive("cart")}
-          type="radio"
-          name="my_tabs_1"
-          className="tab"
-          aria-label={`Cart (${carts.length})`}
-        />
+      <div className="flex justify-center">
+        <div className="tabs tabs-box  rounded-full  border border-gray-200 justify-center bg-white mt-10 ">
+          <input
+            onClick={() => setActive("products")}
+            type="radio"
+            name="my_tabs_1"
+            className={`tab ${active === "products" && " text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full px-5"}`}
+            aria-label="Products"
+            defaultChecked
+          />
+          <input
+            onClick={() => setActive("cart")}
+            type="radio"
+            name="my_tabs_1"
+            className={`tab ${active === "cart" && " text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full px-5"}`}
+            aria-label={`Cart (${carts.length})`}
+          />
+        </div>
       </div>
 
       {active === "products" && (

42e0c83a30a4a9086d3f1c9f4dfce48fecfa769f
mdshantosharker <mdshantosharker@gmail.com>
2026-03-30 10:03:44 +0600

    customize added cart section and ui desing and also responsive

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 const Cart = ({ cart, carts, setCart }) => {
-    
+  const { icon, name, price } = cart;
+
   const handleRemove = (id) => {
     const filterCarts = carts.filter((c) => c.id !== id);
     setCart(filterCarts);
   };
   return (
     <>
-      <div className="flex gap-20 my-10">
-        <h1>Cart{cart.id}</h1>
-        <button onClick={() => handleRemove(cart.id)} className="btn btn-error">
-          Delete
+      <div className="flex justify-between  my-8 bg-[#eef0f1] p-4 rounded-xl">
+        <div className="flex gap-5 items-center">
+          <div className="bg-white rounded-full p-2">
+            <img className="w-8 h-8" src={icon} alt="" />
+          </div>
+          <div>
+            <h1 className="font-bold md:text-xl">{name}</h1>
+            <p className="text-[#627382]">${price}</p>
+          </div>
+        </div>
+
+        <button onClick={() => handleRemove(cart.id)} className="text-[#FF3980] font-bold text-xs lg:text-xl">
+          Remove
         </button>
       </div>
-      
     </>
   );
 };
--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -8,17 +8,27 @@ const Carts = ({ carts, setCart }) => {
   };
   return (
     <>
-      <div>
-        {carts.map((cart) => (
-          <Cart cart={cart} carts={carts} setCart={setCart} />
-        ))}
-      </div>
+      <div className="w-11/12 lg:max-w-7xl mx-auto border border-gray-200 rounded-2xl p-10 space-y-5 my-10">
+        <h1 className=" font-bold text-3xl">Your Cart</h1>
+        <div>
+          {carts.map((cart) => (
+            <Cart cart={cart} carts={carts} setCart={setCart} />
+          ))}
+        </div>
 
-      <div>
-        <h1>Total:{totalPrice}</h1>
-        <button onClick={() => handleProceed()} className="btn btn-accent">
-          Proceed to Checkout
-        </button>
+        <div>
+          <div className="flex justify-between items-center my-5">
+            <h1 className="text-[#627382] text-[16px] ">Total:</h1>
+            <p className="font-bold text-2xl">${totalPrice}</p>
+          </div>
+
+          <button
+            onClick={() => handleProceed()}
+            className="btn text-white w-full py-6 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full px-5"
+          >
+            Proceed to Checkout
+          </button>
+        </div>
       </div>
     </>
   );
